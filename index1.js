91e23f58d8870cd39996d37166280d9d3f892137
Кірюша <mkiril087@gmail.com>
2024-09-28 12:42:07 +0300

    gh

--- index1.js
+++ index1.js
@@ -1,7 +1,78 @@
-let x = 0;
+// task 1
 
-while (x < 10) {
-    console.log(x);
-    x += 1;
+// let x = 1;
+
+
+// while (x <= 10) {
+//     console.log(x);
+//     x += 1;
     
-}
+// }
+
+// task2
+
+// for (i = 0; i <= 20; i += 1) {
+//     if (i % 2 === 1) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+// task 3
+
+// let x = 7;
+
+// for (i = 0; i <= 10; i += 1) {
+//     let sum = x * i;
+//     console.log(sum);
+    
+// }
+
+// task 4
+
+// const nums = [1, 2, 3, 4, 5];
+// let x = 0;
+
+// while (x < 1) {
+//     x += 1;
+//     console.log(nums);
+    
+// }
+
+// task 5
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let x = Math.round(Math.random() * 10);
+
+
+// for (i = 0; x != 8 && i < 10; i += 1) {
+//     if (x === 8) {
+//         break
+//     }
+//     console.log(nums[x]);
+    // console.log(x);
+    
+// }
+
+// task6
+
+// let n = Math.round(Math.random() * 100);
+
+// for (i = 0; i <= n; i += 1) {
+//     console.log(i);
+    
+// }
+
+// task 7
+
+// let x = 20;
+
+// for (i = 0; i <= x; i += 1) {
+//     // x += 1;
+//     if (i % 3 === 0) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
